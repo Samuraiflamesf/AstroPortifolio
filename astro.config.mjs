ddae799e3ac0fd39230f2bd7bd5d27265aa9ecb0
Bernardo Nogueira <62897976+Samuraiflamesf@users.noreply.github.com>
2024-03-04 16:56:05 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://portfolio_Astra.github.io",
-    base: "/my-repo",
+    site: "https://portfolio.github.io",
     integrations: [mdx(), sitemap(), tailwind()],
 });
