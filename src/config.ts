30e0115883943ab385123196054936a280cd5d35
Bernardo Nogueira <Samuraiflamesf@gmail.com>
2024-03-04 22:46:50 -0300

    New pages

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Bernardo Nogueira';
-export const SITE_DESCRIPTION = "Portfólio pessoal criado com Astro e TailwindCSS.";
+export const SITE_DESCRIPTION =
+    "Meu portifolio pessoal criado com as frameworks Astro e TailwindCSS.";
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
